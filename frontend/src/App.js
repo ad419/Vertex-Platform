9f8e1113d10904d55ccc0e347e45531b6cc1a445
ad419 <64656327+ad419@users.noreply.github.com>
2022-02-26 20:08:42 +0100

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./Screens/Home";
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  );
+}
+
+export default App;
